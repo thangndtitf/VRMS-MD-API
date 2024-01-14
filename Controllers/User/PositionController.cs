66f565fd54b473e792f19fc0fd36c7424afd61b4
thangndtitf <thangndtitf@gmail.com>
2024-01-14 13:53:50 +0700

    no message

---
+++ Controllers/User/PositionController.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using VRMS_3layers.Models.ResultObj;
+using VRMS_3Layers.BLL.User;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VRMS_MD.Controllers.User
+{
+    [Route("api/Position")]
+    public class PositionController : Controller
+    {
+        [HttpGet]
+        [Route("/getListPosition")]
+        public ResultObject getListPosition()
+        {
+            return PositionBLL.getListPosition();
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# Vehicle Rental Management System
+# Bộ API cho hệ thống quản lý thông tin của 1 công ty cho thuê xe tải
+
+
+# .NET core 6
+# Entity Framework core
+# PostGresql
