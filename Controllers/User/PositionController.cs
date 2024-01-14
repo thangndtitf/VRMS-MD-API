e345faebc0f3afec7affaa2265b3952e5eddc1dd
thangndtitf <thangndtitf@gmail.com>
2024-01-14 15:02:40 +0700

    no message

--- Controllers/User/PositionController.cs
+++ Controllers/User/PositionController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using VRMS_3layers.Models.ResultObj;
 using VRMS_3Layers.BLL.User;
+using VRMS_3Layers.Models.User;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,6 +16,28 @@ namespace VRMS_MD.Controllers.User
         public ResultObject getListPosition()
         {
             return PositionBLL.getListPosition();
+        }
+
+        [HttpPost]
+        [Route("/insertNewPosition")]
+        public ResultObject insertNewPosition([FromBody] MdPosition mdPosition)
+        {
+            return PositionBLL.insertNewPosition(mdPosition);
+        }
+
+        [HttpPost]
+        [Route("/updatePosition")]
+        public ResultObject updatePosition([FromBody] MdPosition mdPosition)
+        {
+            return PositionBLL.updatePosition(mdPosition);
+        }
+
+
+        [HttpPost]
+        [Route("/deletePosition")]
+        public ResultObject deletePosition([FromBody] decimal positionId)
+        {
+            return PositionBLL.deletePosition(positionId);
         }
     }
 }
