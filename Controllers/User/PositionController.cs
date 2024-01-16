c64c8f73ae1442a1fc5bbf0417f37c492afd3778
thangndtitf <thangndtitf@gmail.com>
2024-01-16 19:15:17 +0700

    no message

--- Controllers/User/PositionController.cs
+++ Controllers/User/PositionController.cs
@@ -1,4 +1,5 @@
 ﻿
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using VRMS_3layers.Models.ResultObj;
 using VRMS_3Layers.BLL.User;
@@ -8,23 +9,25 @@ using VRMS_3Layers.Models.User;
 
 namespace VRMS_MD.Controllers.User
 {
+    
     [Route("api/Position")]
     public class PositionController : Controller
     {
+        [Authorize(Roles = "Admin")]
         [HttpGet]
-        [Route("/getListPosition")]
+        [Route("/getListPosition")]        
         public ResultObject getListPosition()
         {
             return PositionBLL.getListPosition();
         }
-
+        [Authorize(Roles = "Admin")]
         [HttpPost]
         [Route("/insertNewPosition")]
         public ResultObject insertNewPosition([FromBody] MdPosition mdPosition)
         {
             return PositionBLL.insertNewPosition(mdPosition);
         }
-
+        [Authorize(Roles = "Admin")]
         [HttpPost]
         [Route("/updatePosition")]
         public ResultObject updatePosition([FromBody] MdPosition mdPosition)
@@ -32,7 +35,7 @@ namespace VRMS_MD.Controllers.User
             return PositionBLL.updatePosition(mdPosition);
         }
 
-
+        [Authorize(Roles = "Admins")]
         [HttpPost]
         [Route("/deletePosition")]
         public ResultObject deletePosition([FromBody] decimal positionId)
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.OpenApi.Models;
+using VRMS_MD.Auth;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +9,35 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    c.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "basic",
+        In = ParameterLocation.Header,
+        Description = "Basic Authorization header."
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "basic"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+builder.Services.AddAuthentication("BasicAuthentication")
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
 
 var app = builder.Build();
 
@@ -17,6 +49,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
