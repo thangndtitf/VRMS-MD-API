7af8cefc89b0227a9fe6912036bf6891d1aec717
thangndtitf <thangndtitf@gmail.com>
2024-05-08 16:10:43 +0700

    Thang commit 08052024 4

---
+++ Controllers/User/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VRMS_3layers.Models.ResultObj;
+using VRMS_3Layers.BLL.User;
+using VRMS_3Layers.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VRMS_MD.Controllers.User
+{
+    [Route("api/Users")]
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        [Route("/getAllUser")]
+        public ResultObject getListUser()
+        {
+            return UserBLL.GetListUser();
+        }
+
+        [HttpPost]
+        [Route("/AddNewUser")]
+        public ResultObject addNewUser([FromBody]MdUser newUser)
+        {
+            return UserBLL.InsertNewUser(newUser);
+        }
+
+        [HttpPost]
+        [Route("/UpdateUser")]
+        public ResultObject updateUser([FromBody]MdUpdateUser updateUser)
+        {
+            return UserBLL.UpdateUser(updateUser);
+        }
+
+        [HttpPost]
+        [Route("DeleteUser")]
+        public ResultObject deleteUser([FromBody]decimal userName)
+        {
+            return UserBLL.DeleteUser(userName);
+        }
+    }
+}
+
